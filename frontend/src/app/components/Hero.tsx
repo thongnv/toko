f8e11d93a9b06d919345ac3d4d9a1e897b5b5b04
TomNguyen <thongola@hotmail.com>
2023-09-03 14:54:55 +0700

    configure product card

--- frontend/src/app/components/Hero.tsx
+++ frontend/src/app/components/Hero.tsx
@@ -37,7 +37,6 @@ interface HeroProps {
 
 export default function Home({ data }: HeroProps) {
   const products = data.products.data;
-
   return (
     <section>
       <div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
@@ -52,7 +51,7 @@ export default function Home({ data }: HeroProps) {
           <HighlightedText
             text={data.description}
             tag="p"
-            className="tmt-6 mb-8 text-lg sm:mb-12"
+            className="text-lg sm:mb-12"
             color="dark:text-violet-400"
           />
           <section className=" m:py-12 lg:py-24">
--- frontend/src/app/components/Products.tsx
+++ frontend/src/app/components/Products.tsx
@@ -1,37 +1,41 @@
 import Link from "next/link";
 import { Picture } from "../utils/model";
+import Image from "next/image";
+import { getStrapiMedia } from "../utils/api-helpers";
 
 interface ProductsProps {
   products: Product[];
 }
 
 export interface Product {
-  data: {
-    attributes: {
-      name: string;
-      url: string;
-      coverPicture: Picture;
-    }
+  id: number;
+  attributes: {
+    name: string;
+    url: string;
+    cover: {
+      data: {
+          attributes: {
+              url: string;
+          };
+      };
+    };
   }
 }
 
-function ProductCard({ name, url, coverPicture }: any) {
+function ProductCard({ id, attributes }: Product) {
+  console.log(attributes);
+  // const imageUrl = getStrapiMedia(cover.data?.attributes.url);
+  // console.log(imageUrl);
   return (
     <div className="flex flex-col items-center p-4">
-      product here
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-        className="w-8 h-8 dark:text-violet-400"
-      >
-        <path
-          fillRule="evenodd"
-          d="M11.3 1.046A1 1 0 0112 2v5h4a1 1 0 01.82 1.573l-7 10A1 1 0 018 18v-5H4a1 1 0 01-.82-1.573l7-10a1 1 0 011.12-.38z"
-          clipRule="evenodd"
-        ></path>
-      </svg>
-      <h3 className="my-3 text-3xl font-semibold">{name}</h3>
+      {/* <Image
+              className="h-full ml-4"
+              src="{imageUrl}"
+              alt="icon-search"
+              width={20}
+              height={20}
+            /> */}
+      <h3 className="my-3 text-sm font-semibold">{attributes.name}</h3>
       <div className="space-y-1 leading-tight my-6">
       </div>
     </div>
