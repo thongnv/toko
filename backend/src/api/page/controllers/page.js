cb837cd50f73995d03459693fac3437f8a031c09
TomNguyen <thongola@hotmail.com>
2023-09-04 22:59:23 +0700

    deep populate for api pages

--- backend/src/api/page/controllers/page.js
+++ backend/src/api/page/controllers/page.js
@@ -6,4 +6,22 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::page.page');
+module.exports = createCoreController(
+  "api::page.page",
+  ({ strapi }) => ({
+    async find(ctx) {
+      const { query } = ctx;
+
+      const entity = await strapi.entityService.findMany(
+        "api::page.page",
+        {
+          ...query,
+          populate: "deep",
+        }
+      );
+      const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+      return this.transformResponse(sanitizedEntity);
+    },
+  })
+);
