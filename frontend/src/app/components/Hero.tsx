422609fc382c6a893b4572fae2dc0da9113fd432
TomNguyen <thongola@hotmail.com>
2023-09-06 21:00:17 +0700

    minor update card product

--- frontend/src/app/components/Hero.tsx
+++ frontend/src/app/components/Hero.tsx
@@ -40,7 +40,7 @@ export default function Home({ data }: HeroProps) {
   return (
     <section>
       <div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
-        <div className="flex flex-col justify-center p-6 text-center rounded-lg lg:max-w-md xl:max-w-lg lg:text-left">
+        <div className="flex flex-col justify-center p-6 text-center rounded-lg lg:text-left">
           <HighlightedText
             text={data.title}
             tag="h1"
@@ -51,10 +51,10 @@ export default function Home({ data }: HeroProps) {
           <HighlightedText
             text={data.description}
             tag="p"
-            className="text-lg sm:mb-12"
+            className="text-lg"
             color="dark:text-violet-400"
           />
-          <section className=" m:py-12 lg:py-24">
+          <section className="">
             <div className="container mx-auto my-6 grid justify-center gap-4 sm:grid-cols-2 lg:grid-cols-3">
             <Products products={products} />
             </div>
--- frontend/src/app/components/Products.tsx
+++ frontend/src/app/components/Products.tsx
@@ -23,11 +23,9 @@ export interface Product {
 }
 
 function ProductCard({ id, attributes }: Product) {
-  console.log(attributes);
   const imageUrl = getStrapiMedia(attributes.cover.data?.attributes.url);
-  console.log(imageUrl);
   return (
-    <div className="flex flex-col items-center max-w-sm rounded-lg shadowgray-200 rounded-lg shadow">
+    <div className="flex flex-col items-center max-w-sm rounded-lg shadowgray-200 rounded-lg shadow cursor-pointer hover:shadow-lg">
       {imageUrl && (
         <Image
           className="rounded-t-lg w-full"
@@ -37,7 +35,7 @@ function ProductCard({ id, attributes }: Product) {
           height={120}
         />
       )}
-      <h3 className="my-3 text-sm font-semibold">{attributes.name}</h3>
+      <h3 className="my-3 px-2  text-sm font-semibold">{attributes.name}</h3>
       <div className="space-y-1 leading-tight my-6"></div>
     </div>
   );
@@ -45,10 +43,10 @@ function ProductCard({ id, attributes }: Product) {
 
 export default function Products({ products }: ProductsProps) {
   return (
-    <section className=" m:py-12 lg:py-24">
-      <div className="container mx-auto my-6 grid justify-center gap-4 sm:grid-cols-2 lg:grid-cols-3">
-        {products.map((product: Product, index: number) => (
-          <ProductCard key={index} {...product} />
+    <section className="">
+      <div className="container mx-auto my-6 grid justify-center gap-4 sm:grid-cols-2">
+        {products.map((product: Product) => (
+          <ProductCard key={product.id} {...product} />
         ))}
       </div>
     </section>
