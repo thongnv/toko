c7f6e3376e98a233e84ce67e43878be2278925b5
TomNguyen <thongola@hotmail.com>
2023-11-21 22:33:13 +0700

    cart header

--- frontend/src/app/checkout/cart/page.tsx
+++ frontend/src/app/checkout/cart/page.tsx
@@ -2,6 +2,7 @@
 
 import useFromStore from "@/app/hooks/useFromStore";
 import { useCartStore } from "@/app/store/useCartStore";
+import { DeleteOutlined } from "@ant-design/icons";
 
 export default async function ProductRoute() {
   const cart = useFromStore(useCartStore, (state) => state.cart);
@@ -18,22 +19,31 @@ export default async function ProductRoute() {
   return (
     <div className="lg:ml-4 lg:mx-4 mt-4">
       <div className="text-xl font-medium mb-4">GIỎ HÀNG</div>
-      <section className="flex gap-4">
+      <section className="flex gap-4 text-sm">
         {/* content */}
-      <div className="h-full grow md:flex md:flex-col gap-4">
-        <div className="bg-white p-4 rounded-lg">
-          <div className="text-sm font-bold">Màu</div>
+        <div className="h-full grow md:flex md:flex-col gap-4">
+          <div className="bg-white p-4 rounded-lg grid grid-cols-6 grid-flow-col gap-4">
+            <div className="flex gap-2 col-span-2">
+              <input type="checkbox" />
+              <div>Tất cả</div>
+            </div>
+            <div>Đơn giá</div>
+            <div>Số lượng</div>
+            <div>Thành tiền</div>
+            <span className="text-center">
+              <DeleteOutlined rev={undefined} />
+            </span>
+          </div>
+          <div className="bg-white p-4 rounded-lg">
+            <div className="text-xl mb-2">Thông tin chi tiết</div>
+            <ul>
+              {cart?.map((product) => (
+                <li key={product.id}>{product.attributes.name}</li>
+              ))}
+            </ul>
+          </div>
         </div>
-        <div className="bg-white p-4 rounded-lg">
-          <div className="text-xl mb-2">Thông tin chi tiết</div>
-          <ul>
-          {cart?.map((product) => (
-            <li key={product.id}>{product.attributes.name}</li>
-          ))}
-        </ul>
-        </div>
-      </div>
-        
+
         {/* cart */}
         <div className="h-full w-full lg:w-64 lg:block bg-white rounded-lg p-4">
           <div className="mb-4">Tím, 128GB</div>
